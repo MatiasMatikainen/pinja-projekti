df3b962436b1d485e278813f7c80c3a08e999869
Saari <e2202896@edu.vamk.fi>
2024-11-11 15:00:02 +0200

    test

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h2>hello world</h2>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
